dcae115a72a838d47e157dfc939ad72a3220298a
Shaka Willock <shakawillock@gmail.com>
2022-01-04 09:28:02 -0500

    Added a reference to operator, equals, decimal, and clear buttons

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
-const btnNumbers = document.querySelectorAll('.btn-number');
 const calculatorDisplay = document.querySelector('.calculator__display');
+const numberBtns = document.querySelectorAll('.btn-number');
+const operatorBtns = document.querySelectorAll('.btn-operator');
+const btnEquals = document.querySelector('.btn-equals');
+const btnDecimal = document.querySelector('.btn-decimal');
+const btnClear = document.querySelector('.btn-clear');
+let displayValue = '';
 
-btnNumbers.forEach(number => {
-  number.addEventListener('click', changeCalculatorDisplay);
+numberBtns.forEach(number => {
+    number.addEventListener('click', (e) => {
+      displayValue += e.target.textContent;
+      display();
+    });
+});
+
+operatorBtns.forEach(operator => {
+  operator.addEventListener('click', getCalculatorInputs)
 });
 
 function add(number1, number2) {
@@ -22,14 +34,23 @@ function divide(number1, number2) {
 }
 
 function operate(operator, number1, number2) {
-  return operator(number1, number2);
+  if (operator === '+') {
+    displayValue = add(number1, number2)
+  } else if (operator === '-') {
+    displayValue = subtract(number1, number2);
+  } else if (operator === '*') {
+    displayValue = multiply(number1, number2);
+  } else {
+    displayValue = divide(number1, number2);
+  }
 }
 
-function changeCalculatorDisplay(e) {
-  calculatorDisplay.textContent += e.currentTarget.textContent;
+function display() {
+  calculatorDisplay.textContent = displayValue;
 }
 
-function getCalculatorInput(e) {
-   let displayValue = e.currentTarget.textContent;
-   console.log(displayValue)
+function getCalculatorInputs(e) {
+ let operatorValue = e.target.textContent;
+ let firstNumber = displayValue;
+ displayValue = '';
 }
